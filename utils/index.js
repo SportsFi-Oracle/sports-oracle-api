e6092807438e3b02a2514dd41d1b686a5d63d1db
Kevin Kamto <kamtokevin@gmail.com>
2025-01-06 16:01:07 -0500

    refactor: fix for easier imports

--- utils/index.js
+++ utils/index.js
@@ -3,16 +3,14 @@ import { ethers } from "ethers";
 dotenv.config();
 
 // Load environment variables
-const RPC_URL = process.env.RPC_URL; // RPC URL for SX Network or Testnet
-const ORACLE_CONTRACT_ADDRESS = process.env.ORACLE_CONTRACT_ADDRESS; // Deployed SXPriceOracle contract address
-const ORACLE_ABI = [
+export const RPC_URL = process.env.RPC_URL; // RPC URL for SX Network or Testnet
+export const PRIVATE_KEY = process.env.PRIVATE_KEY; // Private key of the account that deployed the oracle contract   
+export const ORACLE_CONTRACT_ADDRESS = process.env.ORACLE_CONTRACT_ADDRESS; // Deployed SXPriceOracle contract address
+export const ORACLE_ABI = [
   "function getPrice(string memory asset) public view returns (uint256)",
   "function lastUpdated() public view returns (uint256)"
 ];
 
 // Setup ethers provider and contract instance
-const provider = new ethers.JsonRpcProvider(RPC_URL);
-const oracleContract = new ethers.Contract(ORACLE_CONTRACT_ADDRESS, ORACLE_ABI, provider);
-
-
-export default oracleContract;
+export const provider = new ethers.JsonRpcProvider(RPC_URL);
+export const oracleContract = new ethers.Contract(ORACLE_CONTRACT_ADDRESS, ORACLE_ABI, provider);
