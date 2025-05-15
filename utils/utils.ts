3a9f1d76eb92e57f05a88dd106ab3cf0583d561a
Kevin Kamto <kamtokevin@gmail.com>
2025-05-15 13:48:12 -0400

    chore: added helper interfaces for reading uniswap pools

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,23 @@
 import { ethers } from "ethers";
 import { RPC_URL } from "./config";
+import { Token } from "@uniswap/sdk-core";
 
 export const readProvider = new ethers.JsonRpcProvider(RPC_URL);
 export const writeProvider = new ethers.JsonRpcProvider(RPC_URL); // Adjust if different network
 export const getSigner = (privateKey: string) => new ethers.Wallet(privateKey, writeProvider);
+
+export interface PoolMetadata {
+    addr: string;
+    dec0: number;
+    dec1: number;
+    token0: Token,
+    token1: Token,
+    invert: boolean;
+  }
+
+  export interface Observation {
+    secondsAgo: number
+    tickCumulative: bigint
+    secondsPerLiquidityCumulativeX128: bigint
+  }
+  
