2678f696206649cb7503013e830239a6025130d6
Kevin Kamto <kamtokevin@gmail.com>
2025-06-17 00:18:12 -0400

    chore: hard coding config file

--- utils/config.ts
+++ utils/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 export const RPC_URL = process.env.RPC_URL || "";
-export const ORACLE_CONTRACT_ADDRESS = process.env.ORACLE_CONTRACT_ADDRESS || "";
+export const ORACLE_CONTRACT_ADDRESS = process.env.ORACLE_CONTRACT_ADDRESS || "0x6ca684b4773aF95AB5AE8d0aB7bA078237536DDF";
 export const TASK_INTERVAL_MS = 45_000;
 export const THRESHOLD = 0.001;
 export const FORCE_AFTER_MS = 10 * 60_000;
